115d63d2411a86aa238a45a513c9c8ea062fb479
Alexandu Dan <dan_lex@yahoo.com>
2013-03-06 00:30:55 +0300

    add comments to AvlTree

--- AvlTree.php
+++ AvlTree.php
@@ -56,6 +56,12 @@ class AvlTree extends BinarySearchTree{
         return 1 + max($this->height($node->getLeftNode()), $this->height($node->getRightNode()));
     }
 
+    /*
+    * Rebalance recursive starting with curent node
+    * Rebalance left subtree, right subtree and current tree 
+    * @param Node $rootNode
+    * return Node $node
+    */
     public function rebalanceRec(Node $rootNode = NULL){
         if ($rootNode === NULL){
            return $rootNode;
@@ -66,40 +72,73 @@ class AvlTree extends BinarySearchTree{
         return $rootNode = $this->rebalanceSubtree($rootNode);
     }
  
+    /*
+    * Rebalance the tree
+    * @return AvlTree
+    */
     public Function rebalance(){
+        //rebalance recursive starting with the root of the tree
         $this->setRoot($this->rebalanceRec($this->getRoot()));
+        return $this;
     }
 
+    /*
+    * Rebalance subtree with root given node
+    * @param Node $rootNode
+    * @return Node $node
+    */
     public function rebalanceSubtree(Node $rootNode = NULL){
        if ($rootNode === NULL){
            return $rootNode;
        }
+       //calculate the balance of the btree with root $rootNode
        $balance = $this->balance($rootNode);
+    
+       //the subtree is balanced, we return the root of the tree
        if ($balance === 0){
            return $rootNode;
        }
-       if ($balance == -2){ 
+      
+       //the subtree is right heavy
+       if ($balance == -2){
+           //calculate the balance of the right subtree
            $balanceSecond = $this->balance($rootNode->getRightNode());
-           if ($balanceSecond >= 1){ 
+
+           //the tree is right left heavy, we perform right rotation on subtree, 
+           //left rotation of the tree
+           if ($balanceSecond >= 1){
+                //rotate right the pivot the right node of the root 
                 $pivot = $rootNode->getRightNode()->rotateRight();
                 $rootNode->setRightNode($pivot);
+                //rotate left with the pivot root
                 $rootNode = $rootNode->rotateLeft();
            }
 
+           //the tree is right right right heavy, we perform simple left rotation
            if ($balanceSecond <= -1){
+                rotate left with the pivot root
                 $rootNode = $rootNode->rotateLeft();
            }
        }
        
+       //the subtree is left heavy
        if ($balance == 2){
+           //calculate the balance of the left subtree
            $balanceSecond = $this->balance($rootNode->getLeftNode());
+
+           //the tree is left left heavy, we perform simple right rotation
            if ($balanceSecond >= 1){
+                //rotate right with the pivot root
                 $rootNode = $rootNode->rotateRight();
            }
 
+           //the tree is left right heavy, we perform left rotation on subtree, 
+	   //right rotation of the tree
            if ($balanceSecond <= -1){
+                //rotate subtree right with pivot the left node of the root 
                 $pivot  = $rootNode->getLeftNode()->rotateLeft();
                 $rootNode->setLeftNode($pivot);
+                //rotate right with the pivot the root
                 $rootNode = $rootNode->rotateRight();
            }
        }
